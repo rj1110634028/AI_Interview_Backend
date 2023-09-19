7b16f95245d5cbd72f9eb14b45cbcd2aa837c1c9
RJ Liu <jo901025@gmail.com>
2023-09-19 21:47:38 +0800

    feat: Add and test script.

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Process;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('json-config', function () {
+    $process = new Process(['python3', config('app.job_script'), base_path() . '/storage/job.json']);
+
+    echo $process->run() . "\n";
+});
